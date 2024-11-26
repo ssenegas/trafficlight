446abe6345a96a254005e4072b01fc95412f39b5
llschall <llschall@gmx.com>
2024-11-26 22:41:53 +0100

    [Model] Create constants

--- src/main/java/org/senegas/trafficlight/view/SerialMessageEmitter.java
+++ src/main/java/org/senegas/trafficlight/view/SerialMessageEmitter.java
@@ -30,7 +30,7 @@ public class SerialMessageEmitter implements PropertyChangeListener {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        if (this.commandSender != null && ! "blinkingState".equals(evt.getPropertyName())) {
+        if (this.commandSender != null && ! TrafficLightModel.BLINKING_STATE.equals(evt.getPropertyName())) {
             String command = this.model.toArduinoCommand();
             this.commandSender.send(command);
         }
