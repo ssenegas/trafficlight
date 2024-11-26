a06626640dd5f99039f0260312bd1e4a51359598
llschall <llschall@gmx.com>
2024-11-26 22:40:38 +0100

    [Model] Create constant

--- src/main/java/org/senegas/trafficlight/model/TrafficLightModel.java
+++ src/main/java/org/senegas/trafficlight/model/TrafficLightModel.java
@@ -6,8 +6,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TrafficLightModel extends AbstractModel implements Serializable {
-	private static final Logger LOGGER = Logger.getLogger(TrafficLightModel.class.getName());
-	
+    private static final Logger LOGGER = Logger.getLogger(TrafficLightModel.class.getName());
+
+    public static final String TURN_ON = "turnOn";
+    public static final String TURN_OFF = "turnOff";
+    public static final String DELAY = "delay";
+    public static final String BLINKING_STATE = "blinkingState";
+
     private TrafficLight trafficLight;
     
     public TrafficLightModel() {
@@ -34,55 +39,55 @@ public class TrafficLightModel extends AbstractModel implements Serializable {
     public void turnOnRed() {
         boolean oldValue = trafficLight.isRedOn();
         trafficLight.turnOn(Color.RED);
-        firePropertyChange("turnOn", oldValue, trafficLight.isRedOn());
+        firePropertyChange(TURN_ON, oldValue, trafficLight.isRedOn());
     }
 
     public void turnOnYellow() {
         boolean oldValue = trafficLight.isYellowOn();
         trafficLight.turnOn(Color.YELLOW);
-        firePropertyChange("turnOn", oldValue, trafficLight.isYellowOn());
+        firePropertyChange(TURN_ON, oldValue, trafficLight.isYellowOn());
     }
 
     public void turnOnGreen() {
         boolean oldValue = trafficLight.isGreenOn();
         trafficLight.turnOn(Color.GREEN);
-        firePropertyChange("turnOn", oldValue, trafficLight.isGreenOn());
+        firePropertyChange(TURN_ON, oldValue, trafficLight.isGreenOn());
     }
 
     public void turnOffRed() {
         boolean oldValue = trafficLight.isRedOn();
         trafficLight.turnOff(Color.RED);
-        firePropertyChange("turnOff", oldValue, trafficLight.isRedOn());
+        firePropertyChange(TURN_OFF, oldValue, trafficLight.isRedOn());
     }
 
     public void turnOffYellow() {
         boolean oldValue = trafficLight.isYellowOn();
         trafficLight.turnOff(Color.YELLOW);
-        firePropertyChange("turnOff", oldValue, trafficLight.isYellowOn());
+        firePropertyChange(TURN_OFF, oldValue, trafficLight.isYellowOn());
     }
 
     public void turnOffGreen() {
         boolean oldValue = trafficLight.isGreenOn();
         trafficLight.turnOff(Color.GREEN);
-        firePropertyChange("turnOff", oldValue, trafficLight.isGreenOn());
+        firePropertyChange(TURN_OFF, oldValue, trafficLight.isGreenOn());
     }
 
     public void setRedDelay(int value) {
         int oldValue = trafficLight.getRedDelay();
         trafficLight.setRedDelay(value);
-        firePropertyChange("delay", oldValue, trafficLight.getRedDelay());
+        firePropertyChange(DELAY, oldValue, trafficLight.getRedDelay());
     }
 
     public void setYellowDelay(int value) {
         int oldValue = trafficLight.getYellowDelay();
         trafficLight.setYellowDelay(value);
-        firePropertyChange("delay", oldValue, trafficLight.getYellowDelay());
+        firePropertyChange(DELAY, oldValue, trafficLight.getYellowDelay());
     }
 
     public void setGreenDelay(int value) {
         int oldValue = trafficLight.getGreenDelay();
         trafficLight.setGreenDelay(value);
-        firePropertyChange("delay", oldValue, trafficLight.getGreenDelay());
+        firePropertyChange(DELAY, oldValue, trafficLight.getGreenDelay());
     }
 
     public boolean isRedOn() {
@@ -113,6 +118,6 @@ public class TrafficLightModel extends AbstractModel implements Serializable {
      * Notify listeners about a property change for the blinking state.
      */
     public void notifyBlinkingStateChanged() {
-        firePropertyChange("blinkingState", null, null);
+        firePropertyChange(BLINKING_STATE, null, null);
     }
 }
